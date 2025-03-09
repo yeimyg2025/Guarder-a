00021b9ddcacae6a44de11ca5f57d634543ea30a
yeimyg2025 <yeimyg833@gmail.com>
2025-03-09 16:18:47 -0400

    implementacion de imagenes

--- presentation/dashboard/dashboard.php
+++ presentation/dashboard/dashboard.php
@@ -12,6 +12,16 @@ include '../Shared/header.php';
                 <div class="row">
                     <div class="col-lg-12">
                         <h1 class="page-header">Menu Inicial</h1>
+                        <img src="../../public/img/colores.jpg" alt="Descripción de la imagen">'
+                        <style>img {
+    width: 500px; /* Tamaño fijo */
+    height: auto; /* Mantiene la proporción */
+    border: 2px solid #640D5F; /* Borde de color */
+    border-radius: 10px; /* Bordes redondeados */
+    justify-self: unset;
+    box-shadow: 5px 5px 15px rgba(0, 0, 0, 0.5); /* Sombra */
+}
+ </style>
                     </div>
                 </div>
             </div>
--- presentation/login/header.php
+++ presentation/login/header.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="">
-    <title>Sistema de pedidos y repartos</title>
+    <title>Guarderia Honguitos</title>
     <link href="../../public/css/bootstrap.min.css" rel="stylesheet">
     <link href="../../public/css/metisMenu.min.css" rel="stylesheet">
     <link href="../../public/css/startmin.css" rel="stylesheet">
